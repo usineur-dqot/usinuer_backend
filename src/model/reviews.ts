e7f5bc24bc855edc5729528b79a5a4d42702a6c8
root <root@ip-172-31-14-146.eu-west-2.compute.internal>
2022-11-15 18:41:27 +0000

    new changes

--- src/model/reviews.ts
+++ src/model/reviews.ts
@@ -3,67 +3,67 @@ import { DataTypes, Model, Optional } from 'sequelize';
 
 export interface reviewsAttributes {
   id: number;
-  comments: string;
-  rating: number;
-  provider_rate1: number;
-  provider_rate2: number;
-  provider_rate3: number;
-  provider_rate4: number;
-  provider_rate5: number;
-  provider_rate6: number;
-  provider_rating_avg: number;
-  emp_rate1: number;
-  emp_rate2: number;
-  emp_rate3: number;
-  emp_rate4: number;
-  emp_rate5: number;
-  emp_rate6: number;
-  emp_rating_avg: number;
-  review_time: number;
-  review_post_date: Date;
-  review_type: '1' | '2';
+  comments?: string;
+  rating?: number;
+  provider_rate1?: number;
+  provider_rate2?: number;
+  provider_rate3?: number;
+  provider_rate4?: number;
+  provider_rate5?: number;
+  provider_rate6?: number;
+  provider_rating_avg?: number;
+  emp_rate1?: number;
+  emp_rate2?: number;
+  emp_rate3?: number;
+  emp_rate4?: number;
+  emp_rate5?: number;
+  emp_rate6?: number;
+  emp_rating_avg?: number;
+  review_time?: number;
+  review_post_date?: Date;
+  review_type?: '1' | '2';
   project_id: number;
   buyer_id: number;
   provider_id: number;
-  hold: '0' | '1';
-  buyer_status: string;
-  provider_status: string;
-  country_code: number;
+  hold?: '0' | '1';
+  buyer_status?: string;
+  provider_status?: string;
+  country_code?: number;
 }
 
 export type reviewsPk = "id";
 export type reviewsId = reviews[reviewsPk];
-export type reviewsOptionalAttributes = "id";
+export type reviewsOptionalAttributes = "id" | "comments" | "rating" | "provider_rate1" | "provider_rate2" | "provider_rate3" | "provider_rate4" | "provider_rate5" | "provider_rate6" | "provider_rating_avg" | "emp_rate1" | "emp_rate2" | "emp_rate3" | "emp_rate4" | "emp_rate5" | "emp_rate6" | "emp_rating_avg" | "review_time" | "review_post_date" | "review_type" | "hold" | "buyer_status" | "provider_status" | "country_code";
 export type reviewsCreationAttributes = Optional<reviewsAttributes, reviewsOptionalAttributes>;
 
 export class reviews extends Model<reviewsAttributes, reviewsCreationAttributes> implements reviewsAttributes {
   id!: number;
-  comments!: string;
-  rating!: number;
-  provider_rate1!: number;
-  provider_rate2!: number;
-  provider_rate3!: number;
-  provider_rate4!: number;
-  provider_rate5!: number;
-  provider_rate6!: number;
-  provider_rating_avg!: number;
-  emp_rate1!: number;
-  emp_rate2!: number;
-  emp_rate3!: number;
-  emp_rate4!: number;
-  emp_rate5!: number;
-  emp_rate6!: number;
-  emp_rating_avg!: number;
-  review_time!: number;
-  review_post_date!: Date;
-  review_type!: '1' | '2';
+  comments?: string;
+  rating?: number;
+  provider_rate1?: number;
+  provider_rate2?: number;
+  provider_rate3?: number;
+  provider_rate4?: number;
+  provider_rate5?: number;
+  provider_rate6?: number;
+  provider_rating_avg?: number;
+  emp_rate1?: number;
+  emp_rate2?: number;
+  emp_rate3?: number;
+  emp_rate4?: number;
+  emp_rate5?: number;
+  emp_rate6?: number;
+  emp_rating_avg?: number;
+  review_time?: number;
+  review_post_date?: Date;
+  review_type?: '1' | '2';
   project_id!: number;
   buyer_id!: number;
   provider_id!: number;
-  hold!: '0' | '1';
-  buyer_status!: string;
-  provider_status!: string;
-  country_code!: number;
+  hold?: '0' | '1';
+  buyer_status?: string;
+  provider_status?: string;
+  country_code?: number;
 
 
   static initModel(sequelize: Sequelize.Sequelize): typeof reviews {
@@ -76,79 +76,79 @@ export class reviews extends Model<reviewsAttributes, reviewsCreationAttributes>
     },
     comments: {
       type: DataTypes.TEXT,
-      allowNull: false
+      allowNull: true
     },
     rating: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     provider_rate1: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     provider_rate2: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     provider_rate3: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     provider_rate4: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     provider_rate5: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     provider_rate6: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     provider_rating_avg: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     emp_rate1: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     emp_rate2: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     emp_rate3: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     emp_rate4: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     emp_rate5: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     emp_rate6: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     emp_rating_avg: {
       type: DataTypes.FLOAT(2,1),
-      allowNull: false
+      allowNull: true
     },
     review_time: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: true
     },
     review_post_date: {
       type: DataTypes.DATE,
-      allowNull: false
+      allowNull: true
     },
     review_type: {
       type: DataTypes.ENUM('1','2'),
-      allowNull: false
+      allowNull: true
     },
     project_id: {
       type: DataTypes.INTEGER,
@@ -164,19 +164,19 @@ export class reviews extends Model<reviewsAttributes, reviewsCreationAttributes>
     },
     hold: {
       type: DataTypes.ENUM('0','1'),
-      allowNull: false
+      allowNull: true
     },
     buyer_status: {
       type: DataTypes.STRING(20),
-      allowNull: false
+      allowNull: true
     },
     provider_status: {
       type: DataTypes.STRING(20),
-      allowNull: false
+      allowNull: true
     },
     country_code: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: true
     }
   }, {
     tableName: 'reviews',
