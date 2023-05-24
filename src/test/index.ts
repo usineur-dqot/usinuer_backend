3318d1b8d58eb0ffa4f1dcbbc8ec1bb1b10d6783
harsh-js <harsh@dqotsolutions.com>
2023-05-24 13:11:29 +0530

    Bug fixes

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,58 +1,23 @@
 import db from "@db/mysql";
 
-import paypal from "paypal-rest-sdk";
-
 const Test = async () => {
 	try {
-		paypal.configure({
-			mode: "sandbox",
-			client_id:
-				"AdttYSVnm8UEVFoLjLFNdUXxpAX8DOZxpXU4QvU50_1X6lTy0lvfO99-dG921aPbbIaVddVZtLs7dcbG",
-			client_secret:
-				"EK6ldhLwJKJRZtSpZO7zXiMMPhFKsH8YLSzQ9kOqgUuBliotfEIt3ImUY9nacz46OQHYlCqhohlnEB8o",
-		});
+		let query: any = await db.sequelize.query(
+			"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE ENGINE = 'MyISAM'",
+		);
+		query = query[0];
 
-		let orderId = "9WN50236EH272062K";
-
-		var create_payment_json = {
-			intent: "sale",
-			payer: {
-				payment_method: "paypal",
-			},
-			redirect_urls: {
-				return_url: "http://return.url",
-				cancel_url: "http://cancel.url",
-			},
-			transactions: [
-				{
-					item_list: {
-						items: [
-							{
-								name: "item",
-								sku: "item",
-								price: "1.00",
-								currency: "USD",
-								quantity: 1,
-							},
-						],
-					},
-					amount: {
-						currency: "USD",
-						total: "1.00",
-					},
-					description: "This is the payment description.",
-				},
-			],
-		};
-
-		paypal.payment.create(create_payment_json, function (error, payment) {
-			if (error) {
-				throw error;
-			} else {
-				console.log("Create Payment Response");
-				console.log(payment);
-			}
-		});
+		for (let q of query) {
+			try {
+				if (q.TABLE_NAME == "groups") {
+				} else {
+					let _query: any = await db.sequelize.query(
+						`ALTER TABLE ${q.TABLE_NAME} ENGINE=INNODB`,
+					);
+					console.log("🚀 ~ file: index.ts ~ line 14 ~ Test ~ _query", _query);
+				}
+			} catch (err) {}
+		}
 	} catch (err) {
 		console.error(err);
 	}
